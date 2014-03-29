1b762825743e302e6847dbd400f4b28ebf2e5fe4
smaknp <smaknp@gmail.com>
2014-03-29 02:35:36 -0400

    StateMachine!

---
+++ PuzzleWizard/src/com/example/puzzlewizard/StateListener.java
@@ -0,0 +1,5 @@
+package com.example.puzzlewizard;
+
+public interface StateListener {
+	public void updateState();
+}
---
+++ PuzzleWizard/src/com/example/puzzlewizard/StateMachine.java
@@ -0,0 +1,37 @@
+package com.example.puzzlewizard;
+
+import java.util.Vector;
+
+public class StateMachine {
+
+	public enum State {Field, Fight, Villager, Instructions, Start};
+	private State curState = State.Start;
+	private Vector<StateListener> listeners = new Vector();
+	
+	StateMachine()
+	{
+		listeners.clear();
+	}
+	
+	public void addObserver(StateListener listener)
+	{
+		listeners.add(listener);
+	}
+	
+	public void notifyObservers(){
+		for(StateListener l : listeners){
+			l.updateState();
+		}
+	}
+	
+	public State getState()
+	{
+		return this.curState;
+	}
+	
+	public void setState(State state)
+	{
+		this.curState = state;
+		this.notifyObservers();
+	}
+}
