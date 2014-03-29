ea28a413bb81a9e17cca2edee02dd1acd7137f37
smaknp <smaknp@gmail.com>
2014-03-29 04:24:40 -0400

    Person!

---
+++ PuzzleWizard/src/com/example/puzzlewizard/Person.java
@@ -0,0 +1,39 @@
+package com.example.puzzlewizard;
+
+import android.app.Activity;
+import android.support.v4.view.MotionEventCompat;
+import android.view.MotionEvent;
+
+public class Person extends Activity{
+
+	private int XP = 10;
+	private Model model = null;
+	
+	Person()
+	{
+		//do nothing for now
+	}
+	
+	public void setModel(Model mod)
+	{
+		this.model = mod;
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event)
+	{
+		int action = MotionEventCompat.getActionMasked(event);
+		
+		//fix up later
+		switch(action)
+		{
+		case (MotionEvent.ACTION_DOWN) :
+		case(MotionEvent.ACTION_MOVE):
+			model.UpdatePerson();
+			return true;
+		default :
+			return true;
+		}
+	}
+	
+}
